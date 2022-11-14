c2fb5fa879ad12c783255d335e1c87384a545e86
yu-retel <dbdudals100@naver.com>
2022-11-14 23:06:18 +0900

    feat: 당첨조건에 따라 비교하는 기능 구현
    -당첨조건에 따라 등수에 맞게 카운트하는 기능
    -카운트 한것을 등수대로 배열로 반환하는 기능

--- src/GameHandler.js
+++ src/GameHandler.js
@@ -14,11 +14,49 @@ class GameHandler {
     const save = [];
     while(lottoCount !== 0) {
       lottoCount -= 1;
-      save.push(Lotto.getRandomNumber());
+      const randomNumber = Lotto.getRandomNumber();
+      if(new Lotto(randomNumber)) {
+        save.push(randomNumber);
+      }
     }
 
     return save;
   }
+
+  static jackpotCountRankMatching(count, bonus, matchList) {
+    if(count === 6) return matchList[Ranklist.first] +=1;
+    if(count === 5 && bonus) {
+      return matchList[Ranklist.second] +=1;
+    }
+    if(count === 5) return matchList[Ranklist.third] +=1;
+    if(count === 4) return matchList[Ranklist.fourth] +=1;
+    if(count === 3) return matchList[Ranklist.fifth] +=1;
+  
+    return matchList;
+  }
+  
+  static hasJackpotCount(buylottos, jackpotNumber, bonusNumber) {
+    const matchList = new Array(5).fill(0);
+    buylottos.map((buylotto) => {
+      let count = 0;
+      let bonus = 0;
+      buylotto.map((lotto) => {
+        jackpotNumber.includes((lotto.toString())) && count++;
+        jackpotNumber.includes(bonusNumber) && bonus++; 
+      });
+      this.jackpotCountRankMatching(count, bonus, matchList);
+    });
+    
+      return matchList;
+  }
 }
+
+const Ranklist = {
+  fifth:0,
+  fourth:1,
+  third:2,
+  second:3,
+  first:4,
+} 
 
 module.exports = GameHandler;
