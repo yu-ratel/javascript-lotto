113e03579ef8ee8c24a626476904fd96350023c8
yu-retel <dbdudals100@naver.com>
2022-11-15 18:55:22 +0900

    style(GameHandler): 코딩컨벤션에 따른 수정
    - 조건문 공백, 세미콜론 누락 등

--- src/GameHandler.js
+++ src/GameHandler.js
@@ -1,22 +1,22 @@
-const Lotto = require("./Lotto");
-const {LOTTO, LOTTO_RANK, NUMBER, RATE_OF_RETURN} = require("./Constant");
+const Lotto = require('./Lotto');
+const { LOTTO, LOTTO_RANK, NUMBER, RATE_OF_RETURN } = require('./Constant');
 
 class GameHandler {
   static randomNumberCount(buyLotto) {
     let count = NUMBER.ZERO;
-    while(buyLotto !== NUMBER.ZERO) {
+    while (buyLotto !== NUMBER.ZERO) {
       buyLotto -= LOTTO.PRICE;
       count += NUMBER.ONE;
     }
-    return count; 
+    return count;
   }
-    
+
   static randomNumberSaveKit(lottoCount) {
     const save = [];
-    while(lottoCount !== NUMBER.ZERO) {
+    while (lottoCount !== NUMBER.ZERO) {
       lottoCount -= NUMBER.ONE;
       const randomNumber = Lotto.getRandomNumber();
-      if(new Lotto(randomNumber)) {
+      if (new Lotto(randomNumber)) {
         save.push(randomNumber);
       }
     }
@@ -25,17 +25,17 @@ class GameHandler {
   }
 
   static jackpotCountRankMatching(count, bonus, matchList) {
-    if(count === NUMBER.SIX) return matchList[LOTTO_RANK.ONE] +=NUMBER.ONE;
-    if(count === NUMBER.FIVE && bonus) {
-      return matchList[LOTTO_RANK.TWO] +=NUMBER.ONE;
+    if (count === NUMBER.SIX) return matchList[LOTTO_RANK.ONE] += NUMBER.ONE;
+    if (count === NUMBER.FIVE && bonus) {
+      return matchList[LOTTO_RANK.TWO] += NUMBER.ONE;
     }
-    if(count === NUMBER.FIVE) return matchList[LOTTO_RANK.THREE] +=NUMBER.ONE;
-    if(count === NUMBER.FOUR) return matchList[LOTTO_RANK.FOUR] +=NUMBER.ONE;
-    if(count === NUMBER.THREE) return matchList[LOTTO_RANK.FIVE] +=NUMBER.ONE;
-  
+    if (count === NUMBER.FIVE) return matchList[LOTTO_RANK.THREE] += NUMBER.ONE;
+    if (count === NUMBER.FOUR) return matchList[LOTTO_RANK.FOUR] += NUMBER.ONE;
+    if (count === NUMBER.THREE) return matchList[LOTTO_RANK.FIVE] += NUMBER.ONE;
+
     return matchList;
   }
-  
+
   static hasJackpotCount(buylottos, jackpotNumber, bonusNumber) {
     const matchList = new Array(NUMBER.FIVE).fill(NUMBER.ZERO);
     buylottos.map((buylotto) => {
@@ -43,12 +43,12 @@ class GameHandler {
       let bonus = NUMBER.ZERO;
       jackpotNumber.split(',').map((jackpot) => {
         buylotto.includes(Number(jackpot)) && count++;
-        jackpotNumber.includes(bonusNumber) && bonus++; 
+        jackpotNumber.includes(bonusNumber) && bonus++;
       });
       this.jackpotCountRankMatching(count, bonus, matchList);
     });
-    
-      return matchList;
+
+    return matchList;
   }
 
   static jackpotMoneyList() {
@@ -58,7 +58,7 @@ class GameHandler {
   static totalJackpotMoney(matchList, monyList) {
     let totalMoney = NUMBER.ZERO;
     matchList.forEach((jackpot, index) => {
-      if(jackpot) {
+      if (jackpot) {
         totalMoney += jackpot * monyList[index];
       }
     });
@@ -66,7 +66,7 @@ class GameHandler {
   }
 
   static rateOfReturnCalculator(totalMoney, buyLottoprice) {
-    return ((totalMoney/buyLottoprice)*RATE_OF_RETURN.MULTIPLY_VALUE).toFixed(NUMBER.ONE);
+    return ((totalMoney / buyLottoprice) * RATE_OF_RETURN.MULTIPLY_VALUE).toFixed(NUMBER.ONE);
   }
 }
 
