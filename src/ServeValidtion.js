d17f829cc442edaff4baeafb6115cbf928d265f6
yu-retel <dbdudals100@naver.com>
2022-11-15 19:16:53 +0900

    style(ServeValidtion): 코딩컨벤션에 따른 수정
    - 연산자 공백, 작은따옴표 등

--- src/ServeValidtion.js
+++ src/ServeValidtion.js
@@ -1,10 +1,10 @@
-const Lotto = require("./Lotto");
-const {LOTTO, NUMBER, ERROR, REGEXP} = require("./Constant");
+const Lotto = require('./Lotto');
+const { LOTTO, NUMBER, ERROR, REGEXP } = require('./Constant');
 
-class ServeValidtion{
+class ServeValidtion {
   static validateLottoBuyPrice(price) {
-    const limit = REGEXP.PRICE_LIMIT; 
-    if (Number(price)%LOTTO.PRICE !== NUMBER.ZERO 
+    const limit = REGEXP.PRICE_LIMIT;
+    if (Number(price)%LOTTO.PRICE !== NUMBER.ZERO
     || !limit.test(price)
     || Number(price) < LOTTO.PRICE) {
       throw new Error(ERROR.LOTTO_PRICE);
@@ -16,20 +16,20 @@ class ServeValidtion{
   static validateJackpotNumber(number) {
     const limits = REGEXP.LOTTO_INPUT_LIMIT;
     number.split(',').map((type) => {
-      if(!limits.test(type)) {
+      if (!limits.test(type)) {
         throw new Error(ERROR.JACKPOT_INPUT_TYPE);
       }
-    })
+    });
   }
 
-  static validateLottoBonusNumber(number , jackpotNumber) {
+  static validateLottoBonusNumber(number, jackpotNumber) {
     Lotto.validateScope(number);
     const limit = REGEXP.LOTTO_INPUT_LIMIT;
-    if(!limit.test(number)) {
-      throw new Error(ERROR.BONUS_ONLY)
+    if (!limit.test(number)) {
+      throw new Error(ERROR.BONUS_ONLY);
     }
-    if(jackpotNumber.includes(number)) {
-      throw new Error(ERROR.BONUS_IS_JACKPOT)
+    if (jackpotNumber.includes(number)) {
+      throw new Error(ERROR.BONUS_IS_JACKPOT);
     }
   }
 }
