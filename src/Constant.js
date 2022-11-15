a35f0a8da86f700364f3631a7c7882d22bcb430b
yu-retel <dbdudals100@naver.com>
2022-11-15 18:54:13 +0900

    style(Constant): 코딩컨벤션에 따른 수정
    - 세미콜론 누락, 매개변수 공백 등

--- src/Constant.js
+++ src/Constant.js
@@ -1,17 +1,17 @@
 const LOTTO = {
-  MIN_NUMBER: 1, 
+  MIN_NUMBER: 1,
   MAX_NUMBER: 45,
   LENGTH: 6,
   PRICE: 1000,
-  JACKPOT_MONEY_LIST: [5000,50000,1500000,30000000,2000000000],
-}
+  JACKPOT_MONEY_LIST: [5000, 50000, 1500000, 30000000, 2000000000],
+};
 
 const PROCESS_MESSAGE = {
   BUY_START: '구입금액을 입력해 주세요.\n',
   BUY_LIST: '개를 구매했습니다.',
   JACKPOT_START: '\n당첨 번호를 입력해 주세요.\n',
   BONUS_START: '\n보너스 번호를 입력해 주세요.\n',
-}
+};
 
 const RESULT_MESSAGE = {
   TOP_TEXT: '당첨 통계',
@@ -22,7 +22,7 @@ const RESULT_MESSAGE = {
   WON_LOTTE_TWO: '5개 일치, 보너스 볼 일치 (30,000,000원) - ',
   WON_LOTTE_ONE: '6개 일치 (2,000,000,000원) - ',
   END: '개',
-}
+};
 
 const LOTTO_RANK = {
   FIVE: 0,
@@ -30,13 +30,13 @@ const LOTTO_RANK = {
   THREE: 2,
   TWO: 3,
   ONE: 4,
-}
+};
 
 const RATE_OF_RETURN = {
   HEADER_TEXT: '총 수익률은 ',
-  FOOTER_TEXT: '%입니다.', 
+  FOOTER_TEXT: '%입니다.',
   MULTIPLY_VALUE: 100,
-}
+};
 
 const NUMBER = {
   ZERO: 0,
@@ -46,12 +46,12 @@ const NUMBER = {
   FOUR: 4,
   FIVE: 5,
   SIX: 6,
-}
+};
 
 const REGEXP = {
   PRICE_LIMIT: /^[1-9][0-9]+$/,
-  LOTTO_INPUT_LIMIT: /^[1-9]+$/, 
-}
+  LOTTO_INPUT_LIMIT: /^[1-9]+$/,
+};
 
 const ERROR = {
   LOTTO_LENGTH: '[ERROR] 로또 번호는 6개여야 합니다.',
@@ -61,7 +61,7 @@ const ERROR = {
   JACKPOT_INPUT_TYPE: '[ERROR] 당첨 번호는 쉼표를 기준으로 숫자로만 입력해야합니다.',
   BONUS_ONLY: '[ERROR] 보너스 번호는 하나의 숫자로 입력해야 합니다.',
   BONUS_IS_JACKPOT: '[ERROR] 보너스 번호는 당첨번호와 중복될 수 없습니다.',
-}
+};
 
 module.exports = {
   LOTTO,
@@ -72,4 +72,4 @@ module.exports = {
   NUMBER,
   REGEXP,
   ERROR,
-}
+};
