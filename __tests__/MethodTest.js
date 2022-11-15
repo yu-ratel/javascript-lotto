f3b1cc4c439b8a83578e488619174a6c7cf74400
yu-retel <dbdudals100@naver.com>
2022-11-15 16:56:01 +0900

    test(MethodTest): 테스트케이스 추가
    - 사용자의 구매금액이 1~9까지의 숫자로 시작되지않고 0이 들어올경우
    - 공백이 포함되어있을경우

--- __tests__/MethodTest.js
+++ __tests__/MethodTest.js
@@ -8,8 +8,14 @@ describe('도메인 로직 작은 단위 테스트', () => {
   test('1000원 단위가 아니라면 오류를 던지는지', () => {
     expect(ServeValidtion.validateLottoBuyPrice('3000')).toBeTruthy();
     expect(() => {
-        ServeValidtion.validateLottoBuyPrice('3200').toThrow();
-    })
+      ServeValidtion.validateLottoBuyPrice('3200')
+    }).toThrow("[ERROR]");
+    expect(() => {
+      ServeValidtion.validateLottoBuyPrice(' 1000')
+    }).toThrow("[ERROR]");
+    expect(() => {
+      ServeValidtion.validateLottoBuyPrice('003000')
+    }).toThrow("[ERROR]");
   })
 
   test('주어진 금액대로 1장당 1000원의 로또의 갯수가 잘 나오는지' , () => {
@@ -23,13 +29,13 @@ describe('도메인 로직 작은 단위 테스트', () => {
   test('보너스 번호는 하나의 수만 허용하는지', () => {
     expect(() => {
       ServeValidtion.validateLottoBonusNumber('12 ')
-    }).toThrow("[ERROR]")
+    }).toThrow("[ERROR]");
     expect(() => {
       ServeValidtion.validateLottoBonusNumber('1a')
-    }).toThrow("[ERROR]")
+    }).toThrow("[ERROR]");
     expect(() => {
       ServeValidtion.validateLottoBonusNumber('12,32')
-    }).toThrow("[ERROR]")
+    }).toThrow("[ERROR]");
   })
 
   test('당첨조건에 따라 등수에 알맞게 배열로 반환해 주는지', () => {
@@ -49,7 +55,7 @@ describe('도메인 로직 작은 단위 테스트', () => {
 
   test('총합값을 토대로 수익률을 소수점 둘째 자리에서 반올림 해주는지', () => {
     expect(GameHandler.rateOfReturnCalculator(5000,9000))
-    .toEqual('55.6');
+    .toEqual('55.6');;
   })
 
 });
