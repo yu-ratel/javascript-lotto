6dbb45d3f9245df9de88f6e377d3c4822b82cda9
yu-retel <dbdudals100@naver.com>
2022-11-15 18:53:31 +0900

    style(App): 코딩컨벤션에 따른 수정
    - 문자열 작은따옴표, 세미콜론 누락 등

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-const LottoGameTool = require("./LottoGameTool");
+const LottoGameTool = require('./LottoGameTool');
 class App {
   constructor() {
-  this.lottoGame = new LottoGameTool;
+    this.lottoGame = new LottoGameTool;
   }
   play() {
     this.lottoGame.lottoBuyStart();
@@ -9,9 +9,3 @@ class App {
 }
 
 module.exports = App;
-
-
-//내일 해야되는 것 
-// 값 -> 상수화 
-// 리팩토링 (어떻게 해야 더 예쁘게?)
-// 소감문 작성 
