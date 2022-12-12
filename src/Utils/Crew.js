5d1d6eba364d51351e57e824440b4ce3603fec31
yu-retel <dbdudals100@naver.com>
2022-12-12 16:42:54 +0900

    feat: 크루 목록을 md파일에서 받아오는 기능구현

---
+++ src/Utils/Crew.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+const randomCrew = (crew) => {
+  return crew.split(' ').sort(() => 0.5 - Math.random());
+}
+
+const frontend = randomCrew(fs.readFileSync('src/resources/frontend-crew.md', 'utf-8'));
+const backend = randomCrew(fs.readFileSync('src/resources/backend-crew.md', 'utf-8'));
+
+console.log(frontend)
+console.log(backend)
